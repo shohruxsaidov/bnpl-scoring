0982906fcacb9befdc284dd0b5d3ab2173e86fe9
shohrux <seltashoh@gmail.com>
2026-07-16 09:48:02 +0500

    refactor: clean up unused imports and streamline channel setup in createContractConsume

--- apps/api/src/modules/merchant/deals/consumers/autopay-creating-contract/create-contract.consumer.ts
+++ apps/api/src/modules/merchant/deals/consumers/autopay-creating-contract/create-contract.consumer.ts
@@ -1,21 +1,16 @@
-import { categories } from './../../../../../db/categories';
 import { ConnectionManager, Channel } from '@lib/rabbit-queue/connection';
 import { createContractHandler } from './create-contract.handler';
-import amqp from 'amqplib';
-import { env } from '@env';
 
 export const createContractConsume = async (connection: ConnectionManager) => {
   const queue = 'merchant-portal@autopay_contract_creating';
-  // const c = await amqp.connect(env.RABBIT_CONNECTION_URL);
-  // const channel = await c.createChannel();
-  // await channel.assertQueue(queue, {
-  //   durable: true,
-  // });
-  // channel.consume(queue, (message) => {
-  //   console.log(message);
-  // });
 
-  const wrapper = connection.createChannel();
+  const wrapper = connection.createChannel({
+    setup(ch: Channel) {
+      return ch.assertQueue(queue, {
+        durable: true,
+      });
+    },
+  });
 
   wrapper.consume(queue, async (msg) => {
     const content = JSON.parse(msg.content.toString()) as any;
