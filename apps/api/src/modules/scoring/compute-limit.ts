02e06397eb76071d90a3430b353d4c1ed72284f2
shohrux <seltashoh@gmail.com>
2026-07-21 12:26:02 +0500

    feat: comment out incomeSum calculations in runModelAndLimit function

--- apps/api/src/modules/scoring/compute-limit.ts
+++ apps/api/src/modules/scoring/compute-limit.ts
@@ -140,9 +140,9 @@ export function runModelAndLimit(input: LimitComputationInput): LimitComputation
       // incomeSum: (inps.incomesAllSumma ?? 0) / 12,
       // incomeSumInBrv: (inps.incomesAllSumma ?? 0) / 12 / BRV_UZS,
       workExperienceMonths: monthsBetween(inps.periodBegin ?? '', inps.periodEnd ?? ''),
-      // TODO
-      incomeSum: 8000000,
-      incomeSumInBrv: 8000000 / 12 / BRV_UZS,
+      // // TODO
+      // incomeSum: 8000000,
+      // incomeSumInBrv: 8000000 / 12 / BRV_UZS,
     }),
     allDebts: 0,
   };
